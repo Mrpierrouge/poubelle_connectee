05bac25eef3eaffc0a1b945ba60d820c2af3536a
Tyro951 <Adam.bakir@edu.devinci.fr>
2024-02-29 12:26:03 +0100

    Crud Update and Delete

--- back/controllers/PoidController.js
+++ back/controllers/PoidController.js
@@ -30,10 +30,34 @@ const createpoid = (req, res) => {
 
 }
 const updatepoid = (req, res) => {
+    let id = Number(req.params.id)
+    let poids = req.body
 
+    prisma.cantines.update({
+        where: {id: id},
+        data: {
+            poids: poids.poids
+        }
+    })
+    .then((poids) =>{
+        res.json(poids)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
 }
+
 const deletepoid = (req,res) =>{
-    
+    let id = Number(req.params.id)
+    prisma.cantines.delete({
+        where: {id: id}
+    })
+    .then((poids) =>{
+        res.json(poids)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
 }
 
 export { getAllpoids, getpoid, createpoid, updatepoid, deletepoid}
