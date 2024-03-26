f416321737229717af1b537a0a0f34243ce737ec
Allia Jarjir <ajarjir92@gmail.com>
2024-03-26 16:43:31 +0100

    Move to front

--- front/src/components/Accueil.vue
+++ front/src/components/Accueil.vue
@@ -64,14 +64,6 @@ nav {
 	height: auto;
 }
 
-.layer {
-	background-color: rgba(255, 255, 255, 0.6);
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-}
 
 .title {
 	text-align: center;
--- front/src/components/Dashboard.vue
+++ front/src/components/Dashboard.vue
@@ -5,7 +5,7 @@
         <img src="../assets/clamart.fr 2_0.svg" alt="Logo" class="logo">
       </nav>
     </header>
-
+<div class="dashboard"></div>
     <div class="dashboard-header">
       <button class="view-all-button">Voir toutes les écoles</button>
       <h1>DASHBOARD</h1>
@@ -72,10 +72,10 @@ export default {
       schools: [
         { name: 'CHARLES DE GAULLE' },
         { name: 'JEAN DE LA FONTAINE' },
-        { name: 'Ecole 3' },
-        { name: 'Ecole 4' },
-        { name: 'Ecole 5' },
-        { name: 'Ecole 6' }
+        { name: 'LOUISE MICHEL' },
+        { name: 'ROCHERS' },
+        { name: 'JEAN MONNET' },
+        { name: 'PLAINE SUD' }
       ],
       selectedSchool: '',
       selectedDate: '',
@@ -93,5 +93,124 @@ export default {
 </script>
 
 <style>
+nav {
+	background-color: #e42353;
+	padding: 10px;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	padding: 10px 20px;
+	box-sizing: border-box;
+}
 
+.navbar {
+	display: flex;
+	justify-content: center;
+}
+
+.logo {
+	width: 100px;
+	height: auto;
+}
+
+.layer {
+	background-color: rgba(255, 255, 255, 0.6);
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+
+.title {
+	text-align: center;
+	margin-top: 20px;
+}
+
+.dashboard-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  width: 100%;
+}
+
+.view-all-button {
+  background-color: rgb(50, 45, 41);
+  color: #fff;
+  border: none;
+  padding: 15px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: transform 0.3s ease;
+}
+
+.view-all-button:hover {
+  transform: scale(1.03);
+}
+
+.dashboard-header h1 {
+  font-size: 24px;
+}
+
+.selectors {
+  display: flex;
+}
+
+.school-selector, .date-selector {
+  margin-left: 10px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.dashboard {
+  padding: 20px;
+}
+
+.dashboard-section {
+  margin-top: 20px;
+}
+
+.dashboard-section h2 {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.dashboard-stats {
+  display: flex;
+  justify-content: space-between;
+}
+
+.stat-box {
+  flex: 1;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: center;
+}
+
+.arrow-down, .arrow-up {
+  width: 20px;
+  height: 20px;
+  margin-top: 10px;
+}
+
+.columns {
+  display: flex;
+}
+
+.column {
+  flex: 1;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: center;
+  margin-right: 10px;
+}
+
+.column:last-child {
+  margin-right: 0;
+}
 </style>
+
